99f328c275ade3198dca945c426bec402d6aae0a
VotedMicrobe606 <levigough93@gmail.com>
2024-10-02 22:46:37 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,40 +1,38 @@
-#bubble(list) - > sorted ist
+def bubbleSort(x):
+    y = len(x)
+    z = x[:]  # Make a copy of the input list
+    for a in range(y):
+        for b in range(y - 1):
+            if z[b] > z[b + 1]:
+                # Swap
+                temp = z[b]
+                z[b] = z[b + 1]
+                z[b + 1] = temp
+    return z
 
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(n - 1):
-            if arr[j] > arr[j + 1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
-    return arr
+def selectionSort(m):
+    n = len(m)
+    p = m[:]  # Make a copy of the input list
+    for q in range(n):
+        minIdx = q
+        for r in range(q + 1, n):
+            if p[r] < p[minIdx]:
+                minIdx = r
+        # Swap
+        temp = p[q]
+        p[q] = p[minIdx]
+        p[minIdx] = temp
+    return p
 
-#selection(list) -> sorted list
-
-def selectionSort(arr):
-    n = len(arr)
-    for i in range(n):
-        minIndex = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[minIndex]:
-                minIndex = j
-        temp = arr[i]
-        arr[i] = arr[minIndex]
-        arr[minIndex] = temp
-    return arr
-
-
-#insertion(list) > sorted list
-
-def insertionSort(arr):
-    n = len(arr)
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
+def insertionSort(s):
+    t = len(s)
+    u = s[:]  # Make a copy of the input list
+    for v in range(1, t):
+        key = u[v]
+        w = v - 1
+        while w >= 0 and u[w] > key:
+            u[w + 1] = u[w]
+            w -= 1
+        u[w + 1] = key
+    return u
 
