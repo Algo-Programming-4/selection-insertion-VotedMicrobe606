f85e68ae981f2beb317501c7bb4e6e30a1b5fe66
VotedMicrobe606 <levigough93@gmail.com>
2024-10-02 22:41:21 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,5 +1,40 @@
 #bubble(list) - > sorted ist
 
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
+
 #selection(list) -> sorted list
 
+def selectionSort(arr):
+    n = len(arr)
+    for i in range(n):
+        minIndex = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+        temp = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+    return arr
+
+
 #insertion(list) > sorted list
+
+def insertionSort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
